848f52832251824c630f119191aa281ea0be35a6
Christine Xiang <yutingcxiang@gmail.com>
2018-07-07 01:33:30 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,10 @@
 def my_each(array) # put argument(s) here
   # code here
   i = 0
+  collection
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
+  return collection
 end
